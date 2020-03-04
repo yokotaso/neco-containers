654130f7662447eea6e031cf20406669e6d44f62
zoetrope <a.ikezoe@gmail.com>
2020-03-04 15:02:39 +0900

    fix

--- demo-todo-app/resources/app/create_table.sh
+++ demo-todo-app/resources/app/create_table.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -h ${ENV_DB_HOST} -P ${ENV_DB_PORT} -u ${ENV_DB_USER} -p${ENV_DB_PASSWORD} < schema.sql
+mysql -h ${ENV_DB_HOST} -P ${ENV_DB_PORT} -u ${ENV_DB_USER} -p${ENV_DB_PASSWORD} < /app/schema.sql
--- demo-todo-app/resources/app/schema.sql
+++ demo-todo-app/resources/app/schema.sql
@@ -1,6 +1,6 @@
 create database if not exists todo;
 
-create table if not exists todo (
+create table if not exists todo.todo (
     todo_id bigint(20) not null AUTO_INCREMENT,
     todo_title varchar(30),
     finished boolean,
