58a9f6ec3b1fb3c1dce4b517b308695a5991ef98
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2020-03-06 03:10:13 +0000

    Add retry for database initialization
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

--- demo-todo-app/resources/app/create_table.sh
+++ demo-todo-app/resources/app/create_table.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mysql -h ${ENV_DB_HOST} -P ${ENV_DB_PORT} -u ${ENV_DB_USER} -p${ENV_DB_PASSWORD} < /app/schema.sql
+NEXT_WAIT_TIME=0
+until mysql -h ${ENV_DB_HOST} -P ${ENV_DB_PORT} -u ${ENV_DB_USER} -p${ENV_DB_PASSWORD} < /app/schema.sql || [ $NEXT_WAIT_TIME -eq 8 ]; do
+   sleep $(( NEXT_WAIT_TIME++ ))
+done
+pause
